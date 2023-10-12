3281a546a7ca193c5a8fc0b3652c94e4b4eaf4dc
Emoncr <biplobemon 75945@gmail.com>
2023-10-12 07:13:17 +0600

    stored user info in localstorage by redux persist

--- clint/src/main.jsx
+++ clint/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor} >
+      <App />
+    </PersistGate>
   </Provider>,
 )
--- clint/src/redux/store.js
+++ clint/src/redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux/user/userSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
 
+//===== Redux Persist's Code ======//
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//===== Redux Store ======//
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+
+  //==== Middlware for serializable check =====//
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
