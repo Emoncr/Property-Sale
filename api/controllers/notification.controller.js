90878fec8804db8b28a1c53e8e02eb80cfb6387b
Emoncr <biplobemon 75945@gmail.com>
2023-11-28 16:21:39 +0600

    created notification get method

--- api/controllers/notification.controller.js
+++ api/controllers/notification.controller.js
@@ -1,11 +1,29 @@
 import Notifications from "../models/notification.model.js";
+import { throwError } from "../utils/error.js";
 
 export const createNotification = async (req, res, next) => {
   if (req.user.id != req.body.notify_from)
     return next(throwError(400, "Token Expired, Login for create post"));
   try {
-    const post = await Notifications.create(req.body);
-    res.status(201).json(post);
+    const createdNotification = await Notifications.create(req.body);
+    res.status(201).json(createdNotification);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+
+//======= Get Notification=======//
+
+export const getNotification = async (req, res, next) => {
+  if (req.user.id != req.params.id)
+    return next(throwError(400, "Token Expired, Login for create post"));
+
+  try {
+    const notification = await Notifications.find({ notify_To: req.params.id });
+    res.status(200).json(notification);
   } catch (error) {
     next(error);
   }
--- api/routes/notification.route.js
+++ api/routes/notification.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createNotification } from "../controllers/notification.controller.js";
+import { createNotification ,getNotification} from "../controllers/notification.controller.js";
 import { verifyToken } from "../utils/varifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createNotification);
+router.get("/:id", verifyToken, getNotification);
 
 export default router;
